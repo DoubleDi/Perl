6f073dd462d9aee0bf91097774f48273f5eadc7d
DoubleDi <Ddenis1@yandex.ru>
2016-06-14 00:03:36 +0300

    structuring the progs

--- progs/Technosfera-perl-master/projects/lj/bin/worker.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env perl
-
-use strict;
-use lib::abs '../lib';
-#BEGIN{GetOptions}
-use Local::Hackathon::Worker $ARGV[0];
-
-my $worker = Local::Hackathon::Worker->new();
-$worker->run();
--- progs/hack/Technosfera-perl-master/projects/lj/bin/server.pl
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env perl
-
-use strict;
-use lib::abs '../lib';
-use Local::Hackathon::Server;
-
-Local::Hackathon::Server->new->run();
